0ab23a8363205be6f54f03df998c295c865700ff
Cindy Germain <cindymarguerite@gmail.com>
2022-03-06 07:55:55 -0700

    commit tree 8ac28e0ab8c101bd55792a692170228e110ba970

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    let newArray =[];
+     for(let i = 0; i < names.length; i++) {
+
+     newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+     }
+
+     return newArray; 
+}
+
+
+function countDown(number){
+
+let i = 0;
+
+while(i <= 10){
+console.log(i++);
+}
+
+}
